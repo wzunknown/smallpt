042cf5fab32f142927ca3db19b3d58112cc1f719
nsigma <wangzhun@pku.edu.cn>
2022-01-21 10:54:48 +0000

    scene: add two new scenes; app/mysmallpt: add spp option;

--- app/mysmallpt.cpp
+++ app/mysmallpt.cpp
@@ -1,7 +1,15 @@
 #include "scene.h"
+#include <cstdio>
 
 int main(int argc, char* argv[]) {
-    Scene sc = argc == 2 ? Scene(argv[1]) : Scene();
+    if (argc < 2) {
+        std::fprintf(stderr, "[*] usage: %s config.yaml [spp]", argv[0]);
+        return 0;
+    }
+    Scene sc(argv[1]);
+    if (argc == 3) {
+        sc.samples_per_pixel = atoi(argv[2]);
+    }
     sc.render();
     sc.save_ppm();
     return 0;
